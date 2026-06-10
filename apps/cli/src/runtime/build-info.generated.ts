1b322696f0192783b63d01482c92796c71b55457
Kevin Londono <kevindario5011@gmail.com>
2026-06-10 15:25:45 -0500

    chore(release): prepare 0.1.5

--- apps/cli/src/runtime/build-info.generated.ts
+++ apps/cli/src/runtime/build-info.generated.ts
@@ -9,7 +9,7 @@ import type { BuildInfo } from "./build-info.js";
 
 export const BUILD_INFO: BuildInfo = {
   version: "0.1.5",
-  commit: "5454628",
+  commit: "e930b21",
   date: "2026-06-10",
   target: "linux-x64",
   channel: "stable",
